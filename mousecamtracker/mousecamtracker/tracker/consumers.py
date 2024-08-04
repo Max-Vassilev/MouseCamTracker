1e3c1730d24ad805e50b4a28efa2c9089ac954a6
Maxim <maximvassilev01@gmail.com>
2024-08-04 17:18:29 +0300

    Minor changes

--- mousecamtracker/mousecamtracker/tracker/consumers.py
+++ mousecamtracker/mousecamtracker/tracker/consumers.py
@@ -5,8 +5,11 @@ class MouseConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
 
-    def receive(self, text_data):        
+    def receive(self, text_data):      
+
         data = json.loads(text_data)
+        # Example of text_data: {"x": 1620,"y": 343}
+
         x = data['x']
         y = data['y']
         self.send(text_data=json.dumps({
