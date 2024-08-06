92cbeceebc509dd6b111a64b0d72bbb15784f3ce
Maxim <maximvassilev01@gmail.com>
2024-08-06 15:55:44 +0300

    Added picture taking functionality to MouseClickConsumer

--- mousecamtracker/mousecamtracker/tracker/consumers.py
+++ mousecamtracker/mousecamtracker/tracker/consumers.py
@@ -1,18 +1,19 @@
 import json
+import cv2
 from channels.generic.websocket import WebsocketConsumer
-from mousecamtracker.tracker.models import *
+from mousecamtracker.tracker.models import MouseData
+import os
+from django.conf import settings
 
 class MouseTrackingConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
 
-    # Called when a message is received from the WebSocket
     def receive(self, text_data):
         data = json.loads(text_data)
         x = data['x']
         y = data['y']
 
-        # Send a JSON-encoded message back to the WebSocket client
         self.send(text_data=json.dumps({
             'x': x,
             'y': y
@@ -28,5 +29,18 @@ class MouseClickConsumer(WebsocketConsumer):
         x = data['x']
         y = data['y']
 
-        # Save the coordinates and a placeholder image path to the database
-        MouseData.objects.create(x_coordinate=x, y_coordinate=y, image_path="test")
+        # Capture image using OpenCV
+        cap = cv2.VideoCapture(0)
+        ret, frame = cap.read()
+        cap.release()
+
+        if ret:
+            image_number = len([f for f in os.listdir(settings.MEDIA_ROOT)]) + 1
+            image_filename = f'mouse_click_{image_number}.png'
+            image_path = os.path.join(settings.MEDIA_ROOT, image_filename)
+
+            # Save the image to the image path
+            cv2.imwrite(image_path, frame)
+
+            # Save the data to the database
+            MouseData.objects.create(x_coordinate=x, y_coordinate=y, image_path=image_filename)
